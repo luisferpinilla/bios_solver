c97d0078caa0543f0351e783a89ecdbb6df89eb6
Luis Pinilla <luisferpinilla@gmail.com>
2023-07-25 02:23:37 -0500

    organizar

--- model.py
+++ model.py
@@ -1,75 +1,34 @@
-import pulp as pu
 from datetime import datetime
 
 from modelador.generador_conjuntos import generar_conjuntos
 from modelador.generador_parametros import generar_parametros
-from modelador.generador_restricciones import generar_restricciones
 from modelador.generador_variables import generar_variables
+from modelador.generador_restricciones import generar_restricciones
 
-file = '/Users/luispinilla/Documents/source_code/bios_solver/model_1.xlsm'
+
+file = '/Users/luispinilla/Dropbox/Trabajos/wa/bios_2023/model_1.xlsm'
 
 
-def construir_parametros(now: datetime, parametros:dict):
-
-    parametros['fecha_inicial'] = now
     
-    generar_conjuntos(parametros, file)
-    
-    generar_parametros(parametros, file, now)
-    
-    return parametros
-
-
-def construir_variables(parametros: dict):
-    
-    return generar_variables(parametros)
-
-
-
-def construir_fob():
-
-    fob = list()
-
-    # Costos por almacenamiento
-
-    # Almacenamiento en puerto por corte de Facturación:
-
-    # Costos por transporte
-
-    # Costo variable de transportar cargas desde puertos hacia plantas
-
-    # Costo fijo de transportar un camion desde puerto hacia plantas
-
-    # Costos por Penalización
-
-    # Costo de no respetar un inventario de seguridad de un ingrediente en una planta
-
-    # Costo de no satisfacer una demanda en una planta
-
-    # Costo por permitir guardar un ingrediente en una planta
-    
-    return fob
-
-
-def resolver():
-    pass
-
-
-def generar_reporte():
-    pass
-
-
 now = datetime(2023, 7, 7)
-
+    
 parametros = dict()
-
+    
+parametros['fecha_inicial'] = now
+    
 generar_conjuntos(parametros=parametros, file=file)
+    
+generar_parametros(parametros=parametros, file=file, now=now)
+    
+variables = generar_variables(parametros)
+        
+restricciones = generar_restricciones(parametros, variables)    
 
-parametros = construir_parametros(now=now, parametros=parametros)
 
-variables = construir_variables(parametros)
 
-restricciones = generar_restricciones(parametros, variables)
+
+
+
 
 
 # Problema
---
+++ modelador/generador_fobjetivo.py
@@ -0,0 +1,26 @@
+import pulp as pu
+
+def construir_fob():
+
+    fob = list()
+
+    # Costos por almacenamiento
+
+    # Almacenamiento en puerto por corte de Facturación:
+
+    # Costos por transporte
+
+    # Costo variable de transportar cargas desde puertos hacia plantas
+
+    # Costo fijo de transportar un camion desde puerto hacia plantas
+
+    # Costos por Penalización
+
+    # Costo de no respetar un inventario de seguridad de un ingrediente en una planta
+
+    # Costo de no satisfacer una demanda en una planta
+
+    # Costo por permitir guardar un ingrediente en una planta
+    
+    return fob
+
