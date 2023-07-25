960a7972ffbe819c1a947c239b87cad75ba2074b
Luis Pinilla <luisferpinilla@gmail.com>
2023-07-25 03:13:15 -0500

    restriccion: balance masa puerto

--- modelador/generador_restricciones.py
+++ modelador/generador_restricciones.py
@@ -2,6 +2,10 @@ import pulp as pu
 
 def _balance_masa_bif(restricciones: list, variables:list, llegadas: list, unidades:list):
 
+    # AR = XPL + XTD
+    
+    restricciones['balance_masa_bif'] = list()
+
     for llegada in llegadas:
 
         campos = llegada.split('_')
@@ -31,7 +35,67 @@ def _balance_masa_bif(restricciones: list, variables:list, llegadas: list, unida
 
             left_expesion.append(xtd_var)
 
-        restricciones.append((pu.lpSum(left_expesion) == ar_val, f'balance bif_{empresa}_{ingrediente}_{puerto}_{barco}_{periodo}'))
+        restricciones['balance_masa_bif'].append((pu.lpSum(left_expesion) == ar_val, f'balance bif_{empresa}_{ingrediente}_{puerto}_{barco}_{periodo}'))
+
+def _inventario_inicial_bodega_puerto(restricciones:list, variables:list):
+    pass
+
+
+def _balance_masa_bodega_puerto(restricciones:list, variables:list, cargas:list, unidades:list):
+    
+    # XIP_{empresa}_{ingrediente}_{puerto}_{barco}_{periodo}
+    # XPL_{empresa}_{ingrediente}_{puerto}_{barco}_{periodo}
+    # XTR_{empresa}_{ingrediente}_{puerto}_{barco}_{ua}_{periodo}
+    
+    # XIP_t-1 + XPL - sum(XTR) = XIP
+    # XIP_t-1 + XPL - sum(XTR) - XIP = 0
+    
+    # carga: empresa_ingrediente_puerto_barco
+    
+    restricciones['balance_masa_bodega_puerto'] = list()
+    
+    for periodo in range(1,30):
+        
+        for carga in cargas:
+            
+            left_expesion = []
+        
+            campos = carga.split('_')
+            empresa = campos[0]
+            ingrediente = campos[1]
+            puerto = campos[2]
+            barco = campos[3]
+            
+            xip_anterior_name = f'XPL_{empresa}_{ingrediente}_{puerto}_{barco}_{periodo-1}'
+            if xip_anterior_name in variables.keys():
+                xip_anterior_var = variables[xip_anterior_name]
+                left_expesion.append(xip_anterior_var)
+            
+            xip_actual_name = f'XPL_{empresa}_{ingrediente}_{puerto}_{barco}_{periodo-1}'
+            if xip_actual_name in variables.keys():
+                xip_actual_var = variables[xip_actual_name]
+                left_expesion.append(xip_actual_var)
+            
+            xpl_name = f'XPL_{empresa}_{ingrediente}_{puerto}_{barco}_{periodo-1}'            
+            if xpl_name in variables.keys():
+                xpl_name_var = variables[xpl_name]
+                left_expesion.append(xpl_name_var)
+
+            
+            for ua in unidades:
+                xtr_name = f'XTR_{empresa}_{ingrediente}_{puerto}_{barco}_{ua}_{periodo}'
+                if xtr_name in variables.keys():
+                    xtr_var = variables[xtr_name]
+                    left_expesion.append(-1*xtr_var)
+            
+            restricciones['balance_masa_bodega_puerto'].append((pu.lpSum(left_expesion) == 0, f'balance_masa_{carga}_{periodo}'))
+              
+        
+        
+        
+    
+    
+
 
 def _satisfaccion_demanda_plantas(restricciones:list, variables:list):
     
@@ -56,16 +120,20 @@ def _capacidad_unidades_almacenamiento():
 
 def generar_restricciones(parametros:list, variables:list):
     
-    restricciones = list()
+    restricciones = dict()
     
     empresas = parametros['conjuntos']['empresas']
     periodos = parametros['conjuntos']['periodos']
     ingredientes = parametros['conjuntos']['ingredientes']
     llegadas = parametros['parametros']['llegadas_cargas']
     unidades = parametros['conjuntos']['unidades_almacenamiento']
+    cargas = parametros['conjuntos']['cargas']
     
     ## Balance de masa en bif    
     _balance_masa_bif(restricciones, variables, llegadas, unidades)
+    
+    ## Balance de masa en Bodega puerto
+    _balance_masa_bodega_puerto(restricciones,variables,cargas, unidades)
     
     # Satisfaccion de la demanda en las plantas
     _satisfaccion_demanda_plantas(restricciones, variables)
