d354be6bfb5dd3405ccb8de611052238dc61d66d
Luis Pinilla <luisferpinilla@gmail.com>
2023-07-25 04:46:56 -0500

    restriccion: satisfaccion de demanda

--- modelador/generador_restricciones.py
+++ modelador/generador_restricciones.py
@@ -37,6 +37,7 @@ def _balance_masa_bif(restricciones: list, variables:list, llegadas: list, unida
 
         restricciones['balance_masa_bif'].append((pu.lpSum(left_expesion) == ar_val, f'balance bif_{empresa}_{ingrediente}_{puerto}_{barco}_{periodo}'))
 
+
 def _inventario_inicial_bodega_puerto(restricciones:list, variables:list, cargas:list, inventario_inicial:list):
     
     # XIP_{empresa}_{ingrediente}_{puerto}_{barco}_{periodo}
@@ -62,7 +63,7 @@ def _inventario_inicial_bodega_puerto(restricciones:list, variables:list, cargas
             else:
                 ip_val = 0.0
                 
-            print(ip_name,"==",ip_val)    
+            # print(ip_name,"==",ip_val)    
             rest = (xip_var == ip_val,f'inventario_inicial_puerto_{carga}')
             
             restricciones['inventario_inicial_puerto'].append(rest)
@@ -118,11 +119,48 @@ def _balance_masa_bodega_puerto(restricciones:list, variables:list, cargas:list,
             restricciones['balance_masa_bodega_puerto'].append((pu.lpSum(left_expesion) == 0, f'balance_masa_{carga}_{periodo}'))
 
 
-def _satisfaccion_demanda_plantas(restricciones:list, variables:list):
+def _satisfaccion_demanda_plantas(restricciones:list, variables:list, plantas:list, ingredientes:list, unidades:list, consumo_proyectado:list):
     
-    # sum(XDM) = DM * BCD
+    # sum(XDM) = DM * (1 -BCD)
+    # sum(XDM) = DM - DM*BCD)
+    # sum(XDM) + DM*BCD = DM
     
-    pass
+    
+    # XDM_{empresa}_{planta}_{unidad}_{ingrediente}_{periodo}
+    # DM_{empresa}_{planta}_{ingrediente}_{periodo}
+    
+    restricciones['Satisfaccion_demanda'] = list()
+
+    for planta in plantas:    
+        for ingrediente in ingredientes:
+            for periodo in range(30):
+            
+                left_expesion = list()
+                
+                dm_name = f'DM_{planta}_{ingrediente}_{periodo}'
+                if dm_name in consumo_proyectado.keys():
+                    dm_value = consumo_proyectado[dm_name]
+                else:
+                    dm_value = 0.0
+                
+                for ua in unidades:
+                    
+                    campos = ua.split('_')
+                    empresa = campos[0]
+                    planta_ua = campos[1]
+                    unidad = campos[2]
+            
+                    if planta == planta_ua:
+    
+                        xdm_name = f'XDM_{empresa}_{planta}_{unidad}_{ingrediente}_{periodo}'
+                        xdm_var = variables[xdm_name]
+                        
+                        left_expesion.append(xdm_var)
+                
+                rest = (pu.lpSum(left_expesion)==dm_value, f'satisfacción_demanda_{dm_name}') 
+                
+                restricciones['Satisfaccion_demanda'].append(rest)            
+
 
 def _mantenimiento_ss_plantas(restricciones:list, variables:list):
     
@@ -145,23 +183,18 @@ def generar_restricciones(parametros:list, variables:list):
     
     # empresas = parametros['conjuntos']['empresas']
     # periodos = parametros['conjuntos']['periodos']
-    # ingredientes = parametros['conjuntos']['ingredientes']
+    plantas = parametros['conjuntos']['plantas']
+    consumo_proyectado = parametros['parametros']['consumo_proyectado']
+    ingredientes = parametros['conjuntos']['ingredientes']
     inventario_inicial = parametros['parametros']['inventario_inicial_cargas']
     llegadas = parametros['parametros']['llegadas_cargas']
     unidades = parametros['conjuntos']['unidades_almacenamiento']
     cargas = parametros['conjuntos']['cargas']
     
-    ## Balance de masa en bif    
-    _balance_masa_bif(restricciones, variables, llegadas, unidades)
-    
-    ## Inventario inicial puerto
-    _inventario_inicial_bodega_puerto(restricciones, variables, cargas, inventario_inicial)
-    
-    ## Balance de masa en Bodega puerto
-    _balance_masa_bodega_puerto(restricciones,variables,cargas, unidades)
+
     
     # Satisfaccion de la demanda en las plantas
-    _satisfaccion_demanda_plantas(restricciones, variables)
+    _satisfaccion_demanda_plantas(restricciones, variables, plantas, ingredientes, unidades, consumo_proyectado)
 
     # Mantenimiento del nivel de seguridad de igredientes en plantas
     _mantenimiento_ss_plantas(restricciones, variables)
@@ -175,8 +208,19 @@ def generar_restricciones(parametros:list, variables:list):
     # Balances de masa de inventarios
     
     ## Balance de masa en cargas en puerto
+    
+    ### Balance de masa en bif    
+    _balance_masa_bif(restricciones, variables, llegadas, unidades)
+    
+    ### Inventario inicial puerto
+    _inventario_inicial_bodega_puerto(restricciones, variables, cargas, inventario_inicial)
+    
+    ### Balance de masa en Bodega puerto
+    _balance_masa_bodega_puerto(restricciones,variables,cargas, unidades)
    
     ## Balance de masa en unidades de almacenamiento por producto en planta
+    
+    
 
     # Asignación de uniades de almacenamiento a ingredientes
     
