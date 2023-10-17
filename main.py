d8e21d72f61cba0035aac181b48d29bf42c3dd9e
Luis Pinilla <luisferpinilla@gmail.com>
2023-10-17 09:34:02 -0500

    Actualizar para ultima version de archivo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from modelo.reporte import Reporte
 
 # if __name__ == '__main__':
 
-file = '0_model_21sept-2_rev.xlsm'
+file = '0_model_12oct-rev.xlsm'
 
 problema = Problema(excel_file_path=file)
 
@@ -21,7 +21,7 @@ problema.generar_target()
 
 problema.gen_constrains()
 
-problema.solve(tlimit=30)
+problema.solve(tlimit=60)
 
 reporte = Reporte(problema=problema)
 
